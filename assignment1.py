21125a8f8bf4832c5cec577f29114b30e5eaf599
cmis <cmis@cmis-hs04.(none)>
2016-02-01 09:55:59 +0700

    I created a file.

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+myName = "Bomin"
+print myName 
+myAgeInYears = 17.1
+print myAgeInYears
+myHeightInMeters = 1.6
+print myHeightInMeters
+sideOfSquareInMeters = 1
+print sideOfSquareInMeters
+lengthOfRectangleInMeters = 3
+heightOfRectangleInMeters = 2
+print lengthOfRectangleInMeters
+print heightOfRectangleInMeters
+
+monthsInYear = 12
+myAgeInMonths = myAgeInYears * monthsInYear
+print myAgeInMonths 
+
+myAgeOfDeath = 100
+yearsLeftToLive = myAgeOfDeath - myAgeInYears
+print yearsLeftToLive
+
+feetInMeter = 3.28084
+myHeightInMeters = myHeightInMeters * feetInMeter
+print myHeightInMeters
+
