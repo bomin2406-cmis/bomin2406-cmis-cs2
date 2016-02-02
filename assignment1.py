3fa1ac97875096dcf40065d6b15aaaa86e43c64e
cmis <cmis@cmis-hs04.(none)>
2016-02-02 11:55:32 +0700

    I edited my file.

--- assignment1.py
+++ assignment1.py
@@ -4,7 +4,7 @@ myAgeInYears = 17.1
 print myAgeInYears
 myHeightInMeters = 1.6
 print myHeightInMeters
-sideOfSquareInMeters = 1
+sideOfSquareInMeters = 9
 print sideOfSquareInMeters
 lengthOfRectangleInMeters = 3
 heightOfRectangleInMeters = 2
@@ -20,6 +20,23 @@ yearsLeftToLive = myAgeOfDeath - myAgeInYears
 print yearsLeftToLive
 
 feetInMeter = 3.28084
-myHeightInMeters = myHeightInMeters * feetInMeter
-print myHeightInMeters
+myHeightInFeet = myHeightInMeters * feetInMeter
+print myHeightInFeet
 
+averageHeightKoreaInMeters = 1.6
+differenceHeightWithAverage = averageHeightKoreaInMeters - myHeightInMeters 
+print differenceHeightWithAverage 
+
+areaOfSquare = sideOfSquareInMeters ** 2
+print areaOfSquare
+
+volumeOfCube = sideOfSquareInMeters ** 3
+halfTheVolumeCube = volumeOfCube / 2
+print halfTheVolumeCube
+
+oneNinthOfArea = areaOfSquare / 9
+print oneNinthOfArea
+
+print "My name is" + " " + str(myName) + "." " " "I am" + " " + str(myAgeInYears) + " " + "years old." " " + "I think I'll die when I am" + " " + str(myAgeOfDeath) + "." " " "I have" + " " + str(yearsLeftToLive) + " " + "years left to live." " " "My height in meters is" + " " + str(myHeightInMeters) + "."
+
+print "My name is" + " " + str(myName) + "." " " "I am" + " " + str(myAgeInYears) + " " + "years old." " " + "I think I'll die when I am" + " " + str(myAgeOfDeath) + "." " " "I have" + " " + str(yearsLeftToLive) + " " + "years left to live." " " "My height in meters is" + " " + str(myHeightInMeters) + "." "In feet, my height is" " " + str(myHeightInFeet) + "." "If one side of a square is " + str(sideOfSquareInMeters) + "," "area of this square is " + str(areaOfSquare) + "." "If it is not a square, and is a cube, then the volume is " + str(volumeOfCube) + "units squared." " " "Half of that volume would be " + str(halfTheVolumeCube) + "."
