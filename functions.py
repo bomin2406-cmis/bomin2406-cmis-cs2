8b54375c25f8aa120315e5c2f53fb789c255e413
cmis <cmis@cmis-hs04.(none)>
2016-02-11 09:58:15 +0700

    I edited a file.

--- functions.py
+++ functions.py
@@ -1,4 +1,39 @@
-def add(a,b):
-    return a + b
+def add(a, b): #compute the addition of a and b
+	return a+b
 
-def 
+def sub(a, b): #compute the difference between a and b
+	return a-b 
+
+def mul(a,b): #compute the multiplication of a and b
+	return a*b
+
+def div(a,b): #creating formula so then the cpu can divide
+	return a/b 
+
+def hours_from_seconds(a): #convert the hours into minutes
+	return a/3600 
+print hours_from_seconds(86400)
+
+import math 
+
+def circle_area(a):
+	return math.pi * (a**2)
+print circle_area(5)
+
+def sphere_volume(a):
+	return 1.33333333333 * math.pi * (a**3) 
+
+print sphere_volume(5)
+
+def avg_volume (a,b):
+	c= a/2
+	d= b/2
+	y= 1.3333333333333*math.pi*c*c*c
+	z= 1.333333333333*math.pi*d*d*d
+	return (y+z)/2
+print avg_volume (10,20)
+
+
+
+
+
