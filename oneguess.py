4fe143aab7b40e4cd828f2236183242a5d5cbcb8
cmis <cmis@cmis-hs04.(none)>
2016-03-15 09:58:12 +0700

    I edited this file.

--- oneguess.py
+++ oneguess.py
@@ -1,16 +1,51 @@
 
-
-def output(miniNumber, maxiNumber, startRange, endRange, guessNumber, targetNumber)
+def output(target, guess, ):
     out = """
-What is the minimum number? {}
-What is the maximum number? {}
-I'm thinking of a number from {} to {}.
-What do you think it is? {}
-
 The target was {}.
 Your guess was {}.
-That's under 
-"""
+That's under by {}.
+""".format(name, x, y, z)
+    return out
+
 
 def main():
-    miniNumber = raw_input
+    miniNo= raw_input("What is the minimum number?:")
+    maxiNo= raw_input("What is the maximum number?:")
+main()
+
+
+def output(miniNo, maxiNo):
+    out = """
+I'm thinking of a number from {} to {}.
+""".format(miniNo, maxiNo)
+
+def main():
+    guess= raw_input("What do you think it is?:")
+main()
+
+
+import random
+
+def randomize(first, second):
+    return random.randint(first, second)
+
+def differenceOfGuessAndTarget(g,t):
+    return abs(g-t)
+
+def output(target, guess):
+    out = """
+The target was {}.
+Your guess was {}.
+That's under by {}.
+""".format(target, guess)
+
+def main():
+    target= randomize(miniNo, maxiNo)
+    d= differenceOfGuessAndTarget(guess,target)
+    out= output(target, guess, d)
+    print out
+main()
+
+
+
+
