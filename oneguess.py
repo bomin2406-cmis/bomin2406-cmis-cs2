b838872cd673da3d75c18f49ac1a235e11c8b008
cmis <cmis@cmis-hs14.(none)>
2016-03-23 11:27:53 +0700

    I edited the file.

--- oneguess.py
+++ oneguess.py
@@ -1,44 +1,36 @@
-def output():
-	out= 
-def main():
-    miniNo= raw_input("What is the minimum number?:")
-    maxiNo= raw_input("What is the maximum number?:")
-	
-main()
-
-def output(target, guess, ):
-    out = """
-The target was {}.
-Your guess was {}.
-That's under by {}.
-""".format(name, x, y, z)
-    return out
-
-def main():
-    guess= raw_input("What do you think it is?:")
-main()
-
+#the raw input (Asking for the guess)
 
 import random
 
 def randomize(first, second):
-    return random.randint(first, second)
+	return random.randint(first, second)
 
 def differenceOfGuessAndTarget(g,t):
-    return abs(g-t)
-
-def output(target, guess):
-    out = """
-The target was {}.
-Your guess was {}.
-That's under by {}.
-""".format(target, guess)
+	return abs(g-t)
 
 def main():
-    target= randomize(miniNo, maxiNo)
-    d= differenceOfGuessAndTarget(guess,target)
-    out= output(target, guess, d)
-    print out
+	miniNo = raw_input("What is the minimum number?:")
+	maxiNo = raw_input("What is the maximum number?:")
+	output = """I'm thinking of a number from {} to {}.""".format(miniNo, maxiNo)
+	print output
+	guess= raw_input("What do you think it is?:")
+
+	target = randomize(int(miniNo), int(maxiNo))
+	difference= abs(int(guess) - int(target))
+
+
+	output= """The target was {}.
+Your guess was {}.
+""".format(target, guess)
+	print output
+	if int(guess) == target:
+		print "YOU GOT IT!"
+	elif int(guess) > target:
+		print "You are over by ", str(abs(int(guess) - target))
+	elif int(guess) < int(target):
+		print "It's under by ", str(abs(int(guess) - target))
+
+
 main()
 
 
