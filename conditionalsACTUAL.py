8acae2cc7afd2c8c76de73134b03c72eb46504fd
cmis <cmis@cmis-hs14.(none)>
2016-03-30 11:17:14 +0700

    I created these.

---
+++ conditionalsACTUAL.py
@@ -0,0 +1,33 @@
+#INTRO
+def output():
+	out = """
+""".format()
+	return out
+
+#About the bomb
+
+
+#Defining the circuits
+def "red" == 1
+
+
+#cutting process
+def main():
+	name = raw_input("So. What's your name..?:")
+	out = """There's someone here, at this hospital full of patients and doctors...tensed up. They are all staring at one thing, and it is, a bomb. The countdown has started: 1 minute remaining. There's no time to spare. That person in the middle is holding it - everything, EVERYTHING depends on him. 
+""".format()
+	return out
+
+	out= """{}. That's you.""".format()
+	return out
+
+	print "You are looking at the bomb right now. There are three circuits attached on the bomb. What are the three colors, from left to right?"
+	color1 = raw_input("Color of the first one?:")
+	color2 = raw_input("Color of the second one?:")
+	color3 = raw_input("Color of the third one?:")
+ 	
+	out= """Th
+	
+
+	
+	
