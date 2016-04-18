eb7b42396a9b3b21970a117cfa0de8ac59153e67
cmis <cmis@cmis-hs23.(none)>
2016-04-18 09:55:39 +0700

    I created this file.

---
+++ ourwakeup.py
@@ -0,0 +1,33 @@
+def main():
+	zerothNo = raw_input("n0:")
+	if float(zerothNo) < 0 or float(zerothNo) >= 10
+		floatedzerothNo = float(zerothNo)
+		print floatedzerothNo " is out of range."
+		
+	else sumOfNos = float(zerothNo)
+	else AnumberOfNos = 1
+
+	1stNo = raw_input("n1:")
+		if float(1stNo) < 0 or float(1stNo) >= 10
+			floated1stNo = float(1stNo)
+			print floated1stNo " is out of range."
+
+		else
+			if float(0thNo) >= 0 or float(0thNo) < 10
+				sumOfNos = float(0thNo) + float(1stNo)
+				BnumberOfNos = AnumberOfNos + 1
+
+	2ndNo = raw_input("n3:")
+		if float(2ndNo) < 0 or float(2ndNo) >= 10
+			floated2ndNo = float(2ndNo)
+			print floated2ndNo " is out of range."
+	3rdNo = raw_input("n4:")
+		if float(3rdNo) < 0 or float(3rdNo) >= 10
+			floated3rdNo = float(3rdNo)
+			print floated3rdNo " is out of range."
+	4thNo = raw_input("n4:")
+		if float(4thNo) < 0 or float(4thNo) >= 10
+			floated4thNo = float(4thNo)
+			print floated4thNo " is out of range."
+
+main()
