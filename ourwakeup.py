90e296a14401d61c05ed554afc6f24f0168f1dc1
cmis <cmis@cmis-hs23.(none)>
2016-04-19 11:45:45 +0700

    I

--- ourwakeup.py
+++ ourwakeup.py
@@ -1,33 +1,46 @@
 def main():
-	zerothNo = raw_input("n0:")
-	if float(zerothNo) < 0 or float(zerothNo) >= 10
-		floatedzerothNo = float(zerothNo)
-		print floatedzerothNo " is out of range."
+	No0 = raw_input("n0:")
+	if float(No0) < 0 or float(No0) >= 10:
+		floatedNo0 = float(No0)
+		print (floatedNo0, " is out of range.")
 		
-	else sumOfNos = float(zerothNo)
-	else AnumberOfNos = 1
+	else sumOfNos = float(zerothNo):
+		AnumberOfNos = 1
 
 	1stNo = raw_input("n1:")
-		if float(1stNo) < 0 or float(1stNo) >= 10
-			floated1stNo = float(1stNo)
-			print floated1stNo " is out of range."
+	if float(1stNo) < 0 or float(1stNo) >= 10:
+		floated1stNo = float(1stNo)
+		print floated1stNo " is out of range."
 
-		else
-			if float(0thNo) >= 0 or float(0thNo) < 10
-				sumOfNos = float(0thNo) + float(1stNo)
-				BnumberOfNos = AnumberOfNos + 1
+	elif float(0thNo) >= 0 or float(0thNo) < 10:
+		sumOfNos = float(0thNo) + float(1stNo)
+		BnumberOfNos = AnumberOfNos + 1
 
 	2ndNo = raw_input("n3:")
-		if float(2ndNo) < 0 or float(2ndNo) >= 10
-			floated2ndNo = float(2ndNo)
-			print floated2ndNo " is out of range."
+	if float(2ndNo) < 0 or float(2ndNo) >= 10:
+		floated2ndNo = float(2ndNo)
+		print floated2ndNo " is out of range."
+
+	elif float(1stNo) >= 0 or float(1stNo) < 10:
+		sumOfNos = float(1stNo) + float(2ndNo)
+		CnumberOfNos = BnumberOfNos + 1
+
 	3rdNo = raw_input("n4:")
-		if float(3rdNo) < 0 or float(3rdNo) >= 10
-			floated3rdNo = float(3rdNo)
-			print floated3rdNo " is out of range."
+	if float(3rdNo) < 0 or float(3rdNo) >= 10:
+		floated3rdNo = float(3rdNo)
+		print floated3rdNo " is out of range."
+
+	elif float(2ndNo) >= 0 or float(2ndNo) < 10:
+		sumOfNos = float(2ndNo) + float(2ndNo)
+		DnumberOfNos = CnumberOfNos + 1
+
 	4thNo = raw_input("n4:")
-		if float(4thNo) < 0 or float(4thNo) >= 10
+		if float(4thNo) < 0 or float(4thNo) >= 10:
 			floated4thNo = float(4thNo)
 			print floated4thNo " is out of range."
+
+		elif float(3rdNo) >= 0 or float(3rdNo) < 10:
+			sumOfNos = float(3rdNo) + float(3rdNo)
+			DnumberOfNos = CnumberOfNos + 1
 
 main()
