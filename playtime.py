976d3d447626c5bec8a7c7c154b72841709afe4b
cmis <cmis@cmis-hs14.(none)>
2016-04-22 09:55:49 +0700

    I created this.

--- playtime.py
+++ playtime.py
@@ -6,14 +6,86 @@ def countup(n):
 		countup(n+1)
 
 def countup_up_from(start, stop):
-	if start == stop:
-		print "BAB"
+	if start >= stop:
+		print "SHOOOOO"
 	else:
 		print start
-		countup(start+1)
+		countup_up_from(start + 1, stop)
+		
+def countdown_from(start, stop):
+	if start <= stop:
+		print "BOOM"
+	else:
+		print start
+		countdown_from(start -1, stop)
+
 
 def main():
-	countup_up_from(2, 15)
-	return
+	countup_up_from(1, 20)
+	countdown_from(20, 1)
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def adder(number):
+	return runningtotal + number
+
+
+intro = "Running total : 0"
+def output1(sumOfNos):
+	out = """
+The sum is {}.""".format(sumOfNos)
+
+def output2(added):
+	out = """
+Next Number: {}
+""".format(added)
+
+
+def adder(number):
+	print intro
+	
+	no = float(raw_input("Next number:"))
+	if no == "":
+		print output1
+	else:
+		added = adder(no)
+		print output2
+
+
+
+def main():
+	print intro
+	
+	no = float(raw_input("Next number:"))
+	if no == "":
+		print output1
+	else:
+		added = adder(no)
+		print output2
 
 main()
+		
+
+
+
+
+
