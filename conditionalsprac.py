339e3783d6295a3872cb4a0d2e64134f13a2a31b
cmis <cmis@cmis-hs14.(none)>
2016-05-09 14:37:25 +0700

    I edited.

---
+++ conditionalsprac.py
@@ -0,0 +1,44 @@
+import random
+
+
+def intro()
+
+def randomize()
+
+def cut1
+
+def cut2
+def intro():
+	name = raw_input("So. What's your name..?:")
+	out = """There's someone here, at this hospital full of patients and doctors...tensed up. They are all staring at one thing, and it is, a bomb. The countdown has started: 1 minute remaining. There's no time to spare. That person in the middle is holding it - everything, EVERYTHING depends on him. 
+""".format()
+	return out
+
+	out= """{}. That's you.""".format()
+	return out
+
+	print "You are looking at the bomb right now. There are three circuits attached on the bomb. Right? And, they are: Red, Blue, and Green."
+
+	cut1 = raw_input("Type the color of the circuit that you would like to cut, ALL IN LOWER CASE:")
+		if cut == "red":
+			cut == 1
+		elif cut == "blue":
+			cut == 2
+		elif cut == "green":
+			cut == 3
+
+	target = random.randint(1, 3)
+	
+		if cut == target:
+			return "YAAASSSSS YOU ARE ALIVE. YAS YAS BUT BUT THE BOMB THOUGH. the timer's still running.....!"
+		else:
+			print "BOOM. YOU'RE DEAD."
+
+	cut2 = raw_input("Next circuit! What color is the circuit you would like to cut now? ALL IN LOWER CASE PLEASE:")
+		if cut2 == target:
+			return "...............the.bomb.is.stopped.YOU WIN BRO."
+		else:
+			print "AW. BOOOOOOOOM. YOU'RE DEAD."
+
+
+
---
+++ yonsup3.py
@@ -0,0 +1,8 @@
+import random
+
+def addition(a, b):
+    a= random.randint(1, 3)
+    b= random.randint(1, 3)
+    return int("a + b")	
+
+print addition
