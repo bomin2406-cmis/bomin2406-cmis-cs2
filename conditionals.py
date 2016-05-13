8f96a2ebc01d2bb0471c637bc25f86bb0f298126
cmis <cmis@cmis-hs14.(none)>
2016-05-13 14:35:32 +0700

    I edited.

--- conditionals.py
+++ conditionals.py
@@ -1,58 +1,103 @@
+import random
 
+def rules():
+	print "HI! I MADE THIS GAME! AND I AM BOMIN!"
+	rulesorno = raw_input:("You wanna hear the rules? (yes/no):")
+	if rulesorno == "yes":
+		print "You CANNOT write in capital letters. ALL of them should be LOWERCASED."
+	else:
+	
+#1. introduction
+def intro():
+	name = raw_input("So. What's your name..?:")
+
+	if name == "Innhye"
+		print "Ew. You can't play this game."
+		exit()
+
+	out = """Clock is ticking. Patients, doctors, staffs,,,,they are all standing still, 
+staring at one thing,,,, and it is, a bomb. The countdown has started: 30 seconds remaining... There's no time to spare. 
+There's a person in the middle, at this hospital full of patients and doctors...tensed up. That person is holding it - everything, EVERYTHING depends on that person. 
+
+{}. That's you.""".format(name)
+	print out
+
+
+
+
+
+#3. first cut!!!
+def firstcut():
+	print "You are looking at the bomb right now. There are three circuits attached on the bomb. Right? And, they are: red, blue, and green."
+
+	cut1 = raw_input("Type the color of the circuit that you would like to cut, ALL IN LOWER CASE:")
+	if cut1 == "red":
+		cut1 = 1
+		return cut1
+	elif cut1 == "blue":
+		cut1 = 2
+		return cut1
+	elif cut1 == "green":
+		cut1 = 3
+		return cut1
+
+
+
+
+#4. dead or alive???!!!
+#	alternative execution
+def deadoralive1(cut1, explosion):
+	if str(cut1) == explosion:
+		print "BOOM. YOU'RE DEAD."
+		exit()
+	else:
+		print "YAAASSSSS YOU ARE ALIVE. YAS YAS BUT BUT THE BOMB THOUGH. the timer's still running.....!"
+
+
+
+
+
+
+#5. second cut!!!!!!!
+def secondcut():
+	print "\nNow you have two circuits left. One will stop the timer, and another one will make it explode...what's your choice?"
+
+	cut2 = raw_input("Type the color of the circuit that you would like to cut, ALL IN LOWER CASE:")
+	if cut2 == "red":
+		cut2 = 1
+		return cut2
+	elif cut2 == "blue":
+		cut2 = 2
+		return cut2
+	elif cut2 == "green":
+		cut2 = 3
+		return cut2
+
+
+
+
+
+
+#6. deadoralive 2!!!!
+#	alternative execution
+def deadoralive2(cut2, explosion):
+	if str(cut2) == explosion:
+		print "BOOM. YOU'RE DEAD."
+	else:
+		print "The timer. is. stopped. YOU'RE ALIVE! EVERYONE IS ALIVE! GOOD JOB!!!"
+
+
+
+
+                                                                                      
+
+#main
 def main():
-	#Find whether if you have a girlfriend or a boyfriend.
-	gender = raw_input("Do have a girlfriend or a boyfriend? -- type g or b:)
-	
-		if gender == g:
-			gOrb== girlfriend
-		if gender == b:
-			gOrb== boyfriend
-
-			if gOrb== girlfriend
-				herOrhis == her
-				sheOrhe == she
-			if gOrb== boyfriend
-				herOrhis == his
-				sheOrhe == he
-
-	output = """Yes. It's happening.
-	In front of you, there's your {} sitting on your chair, OBVIOUSLY being unhappy about something. 
-And, as usual, you don't know why.""".format(gOrb, herOrhis)
-	print output
-
-	#Why mad
-	output = """OK. Now you are thinking. 
-	There are three things that happened yesterday.
-	1. You didn't notice {} haircut.
-	2. You ate {} chocoball, and {} LOVES chocoballs.
-	3. You called {} stupid.
-	""".format(herOrhis, herOrhis, sheOrhe, herOrhis)
-	print output
-
-	thinking1 = "Think. Come ON. THINK. WHAT IS WRONG WITH YOU.
-	thinking2= "It's your fault. It's always your fault."
-	print thinking1
-	print thinking2
-
 	import random
-	def randomize(choice1, choice2, choice3)
-		random.randint(choice1, choice2, choice3)
-
-	yourChoice= raw_input("What do you think is the reason???? 1, 2, or 3? Make it simple, type only the number)
-		if str(randomize(1, 2, 3)) = yourChoice
-			output = """What would you say...?
-1. I...actually noticed it yesterday. You looked too astonishing to response= raw_input("What would you say....?:")
-			
-			
-			
-			
-			
-	
-	
-
-	
-
-	
-	
-
-	
+	explosion = str(random.randint(1, 3))
+	intro()
+	cut1 = firstcut()
+	deadoralive1(cut1, explosion)	
+	cut2 = secondcut()
+	deadoralive2(cut2, explosion)
+main()
