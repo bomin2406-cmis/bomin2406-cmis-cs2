d7ef188ea58e3385b9ef39bc7c12098087131ce8
cmis <cmis@cmis-hs14.(none)>
2016-05-17 09:58:45 +0700

    I edited this file.

--- conditionals.py
+++ conditionals.py
@@ -1,8 +1,13 @@
+#This script runs a game using basic conditional terminology: conditional/alternative/chained executions, raw_input, type conversions, main functions, rational operators, logical operators, random.randint(), and str.format().
+#This is about a game that involves a bomb, where you need to cut the right circuit in order to let the timer stop. Or else, you die. 
+
+
 import random
 
 
 # RULES
 #	conditional execution-1
+#	raw_input
 def rules():
 	print "HI! I MADE THIS GAME! AND I AM BOMIN!"
 	rulesorno = raw_input("You wanna hear the rules? (yes/no):")
@@ -14,6 +19,8 @@ def rules():
 
 #introduction
 #	conditional execution-2
+#	str.format()
+#	""" """
 def intro():
 	name = raw_input("So. What's your name..?:")
 
@@ -33,6 +40,7 @@ There's a person in the middle, at this hospital full of patients and doctors...
 
 
 #3. first cut!!!
+#		chained conditionals -1
 def firstcut():
 	print "You are looking at the bomb right now. There are three circuits attached on the bomb. Right? And, they are: red, blue, and green."
 
@@ -48,15 +56,29 @@ def firstcut():
 		return cut1
 
 
-	if cut1 >= int(explosion) or cut1 <= int(explosion):
-		sureornot = raw_input:("HA. You sure about that? (yes/no):")
-		if sureornot == "yes" and sureornot == "no":
-			cut1 = raw_input:("NO YOU'RE NOT. OF COURSE YOU AREN'T SURE... Type the color again...:")
+
+#You sure?
+#	chained conditionals -2
+def sureornot(cut1):
+	if cut1 > 1 and cut1 < 3:
+		sureornot = raw_input("HA. You sure about that? (yes/no):")
+		if sureornot == "no":
+			cut1 = raw_input("you're not...?? Type the color again...:")
+			if cut1 == "red":
+				cut1 = 1
+				return cut1
+			elif cut1 == "blue":
+				cut1 = 2
+				return cut1
+			elif cut1 == "green":
+				cut1 = 3
+				return cut1
+		else:
+			return cut1
+
 
 	
 	
-
-
 
 
 #4. dead or alive???!!!
@@ -74,19 +96,23 @@ def deadoralive1(cut1, explosion):
 
 
 #5. second cut!!!!!!!
-def secondcut():
+#		chained conditionals-3
+def secondcut(explosion):
 	print "\nNow you have two circuits left. One will stop the timer, and another one will make it explode...what's your choice?"
 
 	cut2 = raw_input("Type the color of the circuit that you would like to cut, ALL IN LOWER CASE:")
+
+	if cut2 > int(explosion) or cut2 < int(explosion) or cut2 == int(explosion):
+		sureornot = raw_input("HA. You sure about that? (yes/no):")
+		if sureornot == "yes" or sureornot == "no":
+			cut2 = raw_input("NO YOU'RE NOT. OF COURSE YOU AREN'T SURE... Type the color again...:")
+
 	if cut2 == "red":
 		cut2 = 1
-		return cut2
 	elif cut2 == "blue":
 		cut2 = 2
-		return cut2
 	elif cut2 == "green":
 		cut2 = 3
-		return cut2
 
 
 
@@ -107,12 +133,16 @@ def deadoralive2(cut2, explosion):
                                                                                       
 
 #main
+#	type conversion
+#	random.randint
 def main():
 	import random
 	explosion = str(random.randint(1, 3))
+	rules()
 	intro()
 	cut1 = firstcut()
+	sureornot(cut1)
 	deadoralive1(cut1, explosion)	
-	cut2 = secondcut()
+	cut2 = secondcut(explosion)
 	deadoralive2(cut2, explosion)
 main()
