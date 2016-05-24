a96c92f122216ba9d72125edd28f243748f1b40e
cmis <cmis@cmis-hs14.(none)>
2016-05-24 09:55:28 +0700

    I edited.

---
+++ whileloops.py
@@ -0,0 +1,27 @@
+
+
+def count_down(beginning):
+	while beginning < 11 and beginning > -1:
+		print beginning
+		beginning -= 1
+
+
+def counter(x):
+	if x > 0:
+		while x > 0:
+			print x
+			x -= 1
+	
+	elif x < 0:
+		while x < 0:
+			print x
+			x += 1
+
+def addup(x):
+	while x
+
+def main():
+#	count_down(10)
+	counter(5)
+
+main()
