815c419c3537d10345c0d177a15559247754fab4
cmis <cmis@cmis-hs14.(none)>
2016-05-25 11:17:28 +0700

    I

--- cs2quiz3.py
+++ cs2quiz3.py
@@ -1,25 +1,37 @@
 #Section 1: Terminology
+
+#POINTS- 1/1
 # 1) What is a recursive function?
 #It is a function that re-runs the fuction until the base case of the function is processed.
 #
 #
+
+#POINTS- 1/1
 # 2) What happens if there is no base case defined in a recursive function?
 #the re-running of the function (by the recursive section) would not be stopped (continuously run through!)
 #
 #
+
+#POINTS- 1/1
 # 3) What is the first thing to consider when designing a recursive function?
 #the base case
 #
 #
+
+#pts- 1/1
 # 4) How do we put data into a function call?
 #using the parameters.
 #
 # 
+
+#pts 1/1
 # 5) How do we get data out of a function call?
 #by returning
 #
 #
 
+
+# 4
 #Section 2: Reading
 # Read the following function definitions and function calls.
 # Then determine the values of the variables a1-d3.
@@ -40,6 +52,9 @@
 #d2 =1
 #d3 =3
 
+
+
+#
 #Section 3: Programming
 #Write a script that asks the user to enter a series of numbers.
 #When the user types in nothing, it should return the average of all the odd numbers
@@ -49,7 +64,14 @@
 #It is NOT NECESSARY to print out a running total with each user input.
 
 
-def adderofodds(number, total):
+# +2 base case is present (MUST BE LABELED) -- 2
+# +2 recursive case is present (MUST BE LABELED) --2
+# +1 base case returns sum/ct (or equivalent) --1
+# +2 recursive case filters even numbers --2
+# +1 recursive case increments sum and ct correctly --0
+# +1 recursive case returns correct recursive call --0
+# +1 main function present AND called  --1
+def avgofodds(number, total):
 	n = raw_input("n: ")
 	if n == "":	
 		out = """The average of the odd numbers is {}""".format(total)
