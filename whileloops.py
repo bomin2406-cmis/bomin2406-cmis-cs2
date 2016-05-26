66cc979c274b5c7e6949437eb44214781d9514bc
cmis <cmis@cmis-hs14.(none)>
2016-05-26 14:35:27 +0700

    I edited.

--- whileloops.py
+++ whileloops.py
@@ -17,11 +17,54 @@ def counter(x):
 			print x
 			x += 1
 
-def addup(x):
-	while x
+#def addup(x):
+#	while x
+
+
+def countFrom(beginning, end):
+	if beginning > end:
+		while beginning > end:
+			print beginning
+			beginning -= 1
+	
+	elif beginning < end:
+		while beginning < end:
+			print beginning
+			beginning += 1
+
+def sumOfOdds(x):
+	total = 0
+	if x > 0:
+		while x > 0:
+			if not x % 2 == 0:
+				total += x
+			else:
+				pass
+			x -= 1
+	
+	elif x < 0:
+		while x < 0:
+			if not x % 2 == 0:
+				total += x
+			else:
+				pass
+			x += 1
+	
+
+	print total
+
+
+		
+
+			
+
+
+	
+
 
 def main():
 #	count_down(10)
-	counter(5)
+#	counter(5)
+	sumOfOdds(9)
 
 main()
